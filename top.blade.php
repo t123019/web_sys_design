eb5442bfd572101b2fa70625a72b829a744fc256
Yukineko1618 <sub6535asterisk@gmail.com>
2025-12-23 11:17:58 +0900

    Add files via upload

---
+++ top.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Food-Expiry-Tracker</title>
+    </head>
+    <body>
+        <h1>Food-Expiry-Tracker</h1>
+
+        <hr>
+
+        <ul>
+            <li>
+                <a href="{{ route('images.index') }}">
+                    食品一覧を見る
+                </a>
+            </li>
+            <li>
+                <a href="{{ route('images.note') }}">
+                    賞味期限通知を見る
+                </a>
+            </li>
+        </ul>
+
+        <hr>
+    </body>
+</html>
